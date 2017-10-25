b5593314c9273723154b7f81020fca9a0a66307e
alex850111 <alex850111@yahoo.com.tw>
2017-10-26 00:05:12 +0800

    first commit

---
+++ src/com/data/jdba/DataBaseAccess.java
@@ -0,0 +1,28 @@
+package com.data.jdba;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DataBaseAccess {
+	private final String datasource = "jdbc:mysql://1.171.32.6:3307/test?user=javauser&password=0000&useSSL=false";;
+	private Connection conn = null;
+	private Statement st;
+	private ResultSet rs;
+	
+ 
+	
+	
+	public static enum targetTable{USER, GAME, FAVORITE, MESSAGE};
+	
+	public DataBaseAccess() throws ClassNotFoundException, SQLException{
+		 Class.forName("com.mysql.jdbc.Driver");
+		 conn = DriverManager.getConnection(datasource);
+		 st = conn.createStatement();
+	}
+	
+ 
+	
+}

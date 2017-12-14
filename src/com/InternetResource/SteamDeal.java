fb1605d51b902d58faba5143456da32252d2f3c1
alex850111 <alex850111@yahoo.com.tw>
2017-12-15 01:11:06 +0800

    未測試

--- src/com/InternetResource/SteamDeal.java
+++ src/com/InternetResource/SteamDeal.java
@@ -5,32 +5,62 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import org.json.JSONObject;
+
 import com.Database.DataBaseManager;
 
 public class SteamDeal {
-	private final String steamSaleUrl = "store.steampowered.com/api/appdetails?appids=";
+	private final String steamDetailUrl = "store.steampowered.com/api/appdetails?appids=";
+	private final String steamGameLinkBase = "http://store.steampowered.com/app/";
 	private String JSONToken[] = {"", "data", "price_overview", "discount_percent"}; 
 	
 	public SteamDeal() {
 	}
 	
 	public String isGameInDeal(String title){
-		String s = ""; 
+		String discountMessage = ""; 
 		try{
 			ArrayList<String> idList = DataBaseManager.matchGameTitle(title);
-		}catch(SQLException e){
-			s = title + "目前沒有特價喔";
+			for(String id : idList){
+				URL website = new URL(steamDetailUrl);
+				ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+				FileOutputStream fos = new FileOutputStream("temp.json");
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+				fos.close();
+				//download app detail json file
+				
+				String temp = "";
+				BufferedReader mbr = new BufferedReader(new InputStreamReader(new FileInputStream("temp.json"), "UTF-8"));
+				while (mbr.ready())
+					temp += mbr.readLine();
+				mbr.close();
+				//read app detail json file
+				
+				//start parse json
+				JSONObject jsonData = new JSONObject(temp).getJSONObject(id).getJSONObject("data");
+				int discountRate = jsonData.getJSONObject("price_overview").getInt("discount_percent");
+				//skip no discount item
+				if(discountRate == 0)continue;
+				String gameTitle = jsonData.getString("name");
+				
+				discountMessage += discountRate + "%:" + gameTitle + ":" + steamGameLinkBase + id + "\n";
+			}
+		}catch(Exception e){
+			discountMessage = "我現在不太方便查欸";
 			e.printStackTrace();
 		}
 		
-		//JSONToken[0] = ;
-		return s;
+		if(discountMessage.isEmpty())
+			discountMessage = title + "現在沒有特價喔";
+		
+		return discountMessage;
 	} 
 	
 
